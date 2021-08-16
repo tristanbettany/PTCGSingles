ec7e046363d2309d5f542625b205d9cfdca61bba
Tristan Bettany <tristan.bettany@gmail.com>
2021-08-16 15:37:56 +0100

    Spoof user agent

--- app/Scrapers/AbstractScraper.php
+++ app/Scrapers/AbstractScraper.php
@@ -51,7 +51,12 @@ abstract class AbstractScraper implements ScraperInterface
 
         $response = $this->guzzle->request(
             'GET',
-            $url
+            $url,
+            [
+                'headers' => [
+                    'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36',
+                ],
+            ]
         );
 
         return $response->getBody()->getContents();
