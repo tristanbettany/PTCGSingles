3eaa0a9806314dba8f074a6335b420ac941b86b4
Tristan Bettany <tristan.bettany@gmail.com>
2021-08-29 07:58:14 +0100

    Bulk value scraper

---
+++ app/Console/Commands/ScrapeValuesCommand.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Interfaces\CardVersionServiceInterface;
+use App\Models\ReleasedCardVersion;
+use Illuminate\Console\Command;
+
+class ScrapeValuesCommand extends Command
+{
+    protected $signature = 'scrape:values';
+    protected $description = 'Scrape Values';
+
+    private CardVersionServiceInterface $cardVersionService;
+
+    public function handle(
+        CardVersionServiceInterface $cardVersionService
+    ): int {
+        $this->cardVersionService = $cardVersionService;
+
+        $this->info('Scraping values...');
+
+        $versions = ReleasedCardVersion::query()
+            ->where('value', null)
+            ->orWhere('value', '<=', 0)
+            ->get();
+
+        foreach($versions as $version) {
+            $card = $version->releasedCard;
+            $cardNumber = $card->paddedNumber();
+            $cardName = $card->name;
+            $setName = $card->set->name;
+
+            echo "Searching '$setName $cardName $cardNumber'\n";
+
+            $value = $this->cardVersionService->scrapeValue($version->id);
+
+            if ($value > 0) {
+                echo "Value is: £$value\n";
+            } else {
+                echo "Value not found\n";
+            }
+        }
+
+        $this->info('Done');
+
+        return 0;
+    }
+}
